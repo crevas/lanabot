52e9b0f74eab9f41e0c8db5c418e8710c4330f22
crevas <89767075+crevas@users.noreply.github.com>
2021-11-09 08:59:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 
 client = discord.Client()
 
@@ -10,6 +11,20 @@ COMMAND_PREFIX_ke = "fkskdi"
 async def on_ready():
   print('Ready')
   await client.change_presence(status=discord.Status.online, activity=discord.Game(name="새롭게 태어나려고"))
+
+@client.event
+async def on_message():
+  if str(message.channel.type) == "text":
+    if message.content == "라나야" or message.content == "fkskdi":
+      rm = random.randint(1, 4)
+      if rm == 1:
+        await message.channel.send("네?")
+      if rm == 2:
+        await message.channel.send("왜요?")
+      if rm == 3:
+        await message.channel.send("저요?")
+      if rm == 4:
+        await message.channel.send("저랑 놀아주세요!")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
