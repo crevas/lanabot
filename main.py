5a7ad409715775c005aa7e42de9c7cdcf7e6d487
crevas <89767075+crevas@users.noreply.github.com>
2021-11-09 14:18:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ import discord
 import os
 import random
 import json
+import sqlite3
+
+# DB 생성
+#con = sqlite3.connect("memory.db")
+# 커서 획득
+#cur = con.cursor()
+#예제 sql문
+#sql1="CREATE TABLE ark(Id text,name text);"
+#sql2="INSERT INTO ark Values('gain_time@naver.com','myblog4');"
+#sql3="INSERT INTO ark Values('gain_time@naver.com','myblog4');"
+#sql4="INSERT INTO ark Values('gain_time@naver.com','myblog4');"
+#sql5="INSERT INTO ark Values('123123@naver.com','myblog4');"
+#sql6="INSERT INTO ark Values('444241@naver.com','myblog4');"
+#sql실행.
+#cur.execute(sql1)
+#cur.execute(sql2)
+#cur.execute(sql3)
+#cur.execute(sql4)
+#cur.execute(sql5)
+#cur.execute(sql6)
+#commit
+#con.commit()
+#데이터 확인해보기.
+#cur.execute('SELECT * FROM ark')
+#for row in cur:
+#  print(row)
+#db연결해제.
+#con.close()
 
 client = discord.Client()
 
@@ -35,7 +63,12 @@ async def on_message(message):
         embed.set_footer(text="made by crevas")
         await message.channel.send(embed=embed)
       if message.content.split(" ")[1] == "배워":
-        
+        con = sqlite3.connect("memory.db")
+        cur = con.cursor()
+        sql1="CREATE TABLE ark(in text,out text);"
+        sql2="INSERT INTO ark Values(message.content.split(" ")[2], message.content.split(" ")[3]);"
+        cur.execute(sql1)
+        cur.execute(sql2)
       
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
