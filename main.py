4ab9b3f7571b01dd5026cb5e3e88e6531f246f1e
crevas <89767075+crevas@users.noreply.github.com>
2021-11-09 13:11:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 
 client = discord.Client()
 
-COMMAND_PREFIX_kk = "라나야"
-COMMAND_PREFIX_ke = "fkskdi"
+EVENT_PREFIX = "라나야"
+COMMAND_PREFIX = "l/"
 
 @client.event
 async def on_ready():
@@ -17,8 +17,8 @@ async def on_message(message):
   if message.author.bot:
     return None
   if message.content == "테스트":
-    await message.channel.send("테스트 중입니다!")
-  if message.content == "라나야" or message.content == "fkskdi":
+    await message.channel.send("정상 작동 중!")
+  if message.content == EVENT_PREFIX:
     rm = random.randint(1, 4)
     if rm == 1:
       await message.channel.send("네?")
@@ -28,6 +28,11 @@ async def on_message(message):
       await message.channel.send("저요?")
     if rm == 4:
       await message.channel.send("저랑 놀아주세요!")
+  if message.content.split(" ")[0] == EVENT_PREFIX:
+    if message.content.split(" ")[1] == "초대":
+      embed = discord.Embed(title="라나 초대!", description="라나와 함께 놀고 싶다면 아래 링크 클릭!", color=0x0FF1CE)
+      embed.add_field(name=':link: https://discord.com/api/oauth2/authorize?client_id=841739951040888844&permissions=8&scope=bot', inline=false)
+      embed.set_footer(text="made by crevas",image_url="https://cdn.discordapp.com/attachments/907481613913436180/907481813591662592/130_20210809091743.png")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
